e959e118e59b281a96566e3598b6c076c25a100e
Kyusung Sohn <rok.ksohn@gmail.com>
2025-11-30 17:08:44 +0900

    Style: table header UI sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./components/ui/button";
+import { Table, TableHead, TableHeader } from "./components/ui/table";
 
 const App = () => {
   return (
@@ -8,7 +9,31 @@ const App = () => {
         <Button className="mt-auto w-52">계산하기</Button>
       </div>
       <div className="flex-1 p-1">
-        <div className="h-full w-full rounded-2xl bg-zinc-900"></div>
+        <div className="h-full w-full rounded-2xl bg-zinc-900 text-white">
+          <Table>
+            <TableHeader className="flex border-b border-zinc-700">
+              <p className="w-[150px] py-2 text-center text-base font-bold">
+                구분
+              </p>
+              <TableHead className="flex-1 py-2 text-center text-base font-bold text-white">
+                자산군
+              </TableHead>
+              <TableHead className="flex-1 py-2 text-center text-base font-bold text-white">
+                보유
+              </TableHead>
+              <TableHead className="flex-1 py-2 text-center text-base font-bold text-white">
+                현재 비율
+              </TableHead>
+              <TableHead className="flex-1 py-2 text-center text-base font-bold text-white">
+                성장형
+              </TableHead>
+              <TableHead className="flex-1 py-2 text-center text-base font-bold text-white">
+                안정형
+              </TableHead>
+            </TableHeader>
+            {/* <TableBody></TableBody> */}
+          </Table>
+        </div>
       </div>
     </div>
   );
