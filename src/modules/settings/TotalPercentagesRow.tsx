bccedce67e9e0f50d2bf3491acc1e5cc3c11a329
Kyusung Sohn <rok.ksohn@gmail.com>
2026-02-23 12:43:54 +0900

    Style: colorgrade accumulated percentages

--- src/modules/settings/TotalPercentagesRow.tsx
+++ src/modules/settings/TotalPercentagesRow.tsx
@@ -1,6 +1,18 @@
 import { usePortfolioContext } from "@/context";
 import { getTotalPercentages } from "@/utils";
 
+const getPercentageColor = (percentage: number) => {
+  if (percentage < 50) {
+    return "text-red-500";
+  } else if (percentage <= 60) {
+    return "text-orange-500";
+  } else if (percentage <= 99) {
+    return "text-yellow-500";
+  } else {
+    return "text-green-500";
+  }
+};
+
 export const TotalPercentagesRow = () => {
   const { portfolio } = usePortfolioContext();
   const { stable, growth } = getTotalPercentages(portfolio);
@@ -9,10 +21,16 @@ export const TotalPercentagesRow = () => {
     <li className="flex flex-col gap-5 rounded-xl border border-zinc-300 bg-white p-3">
       <div className="flex items-start justify-between">
         <span className="text-zinc-700">합계</span>
-        <div className="flex gap-1 font-bold text-zinc-700">
-          <span>안정: {stable}%</span>
-          <span>/</span>
-          <span>성장: {growth}%</span>
+        <div className="flex gap-1 font-bold text-zinc-500">
+          <div className="flex gap-1">
+            <span>안정:</span>
+            <span className={getPercentageColor(stable)}>{stable}%</span>
+          </div>
+          <span> / </span>
+          <div className="flex gap-1">
+            <span>성장:</span>
+            <span className={getPercentageColor(growth)}>{growth}%</span>
+          </div>
         </div>
       </div>
     </li>
