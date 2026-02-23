dcaab7692c47967e047505481578306a23c35035
Kyusung Sohn <rok.ksohn@gmail.com>
2026-02-23 22:19:24 +0900

    Chore: fix base portfolio values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,8 +16,8 @@ export const DEFAULT_PORTFOLIO: Portfolio = [
     quantity: 0,
   },
   {
-    name: "인도주식/원자재",
-    description: "(타이거 인도니프티S0) (WT원유)",
+    name: "인도주식 / 원자재",
+    description: "(TIGER 인도니프티 50 / WT 원유)",
     category: "stocks",
     growth: 10,
     stable: 10,
@@ -43,8 +43,8 @@ export const DEFAULT_PORTFOLIO: Portfolio = [
     quantity: 0,
   },
   {
-    name: "미국채 10년 (UH)",
-    description: "(KODEX 미국10년국채선물)",
+    name: "미국채 30년 (UH)",
+    description: "(ACE 미국30년국채액티브)",
     category: "bonds",
     growth: 25,
     stable: 0,
@@ -53,7 +53,7 @@ export const DEFAULT_PORTFOLIO: Portfolio = [
   },
   {
     name: "현금성자산",
-    description: "(TIGER KOFR금리액티브(합성))",
+    description: "(ACE 미국달러SOFRA금리(합성))",
     category: "cash",
     growth: 15,
     stable: 25,
